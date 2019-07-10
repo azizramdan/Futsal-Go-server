f5cd972629ec495d047fc1757d74b1138ffd46d6
azizramdan <azizramdan44@gmail.com>
2019-07-10 19:26:23 +0700

    fix login

--- user.php
+++ user.php
@@ -1,46 +1,66 @@
 <?php
-if($_SERVER['REQUEST_METHOD']=='POST') {
+if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['method'])) {
 	//include file connect.php untuk menyambungkan file create.php dengan database
-	include "configuration.php";
+	include_once "configuration.php";
 	//inisialisasi variabel method
 	$method = $_POST['method'];
+	//inisialisasi variabel untuk menampung data
+	$response;
 
-	if($method=='login') {
-		//inisialisasi variabel yang akan ditampung dan diolah dengan query
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		//inisialiasi query cek akun
-		$query = "SELECT * FROM user WHERE email = '$email'";
-		//pemanggilan fungsi mysqli_query untuk mengirimkan perintah sesuai parameter yang diisi
-		$result = $conn->query($query);
-		//pengkondisian saat fungsi mysqli_query berhasil atau gagal dieksekusi
-		if ($result->num_rows === 1) {
-			$result = $result->fetch_assoc();
-			if (password_verify($password, $result['password'])) {
-				$response = array(
-					'status' => TRUE,
-					'msg' => 'Login berhasil',
-					'data' => array(
-						'id' => $result['id'],
-						'nama' => $result['nama'],
-						'email' => $result['email']
-					)
-				);
-			} else {
-				$response = array(
-					'status' => FALSE,
-					'msg' => 'Password salah!'
-				);
-			}
+	if($method == 'login') {
+		login();
+	} else if($method == 'registrasi') {
+		registrasi();
+	} else if($method == 'update') {
+		update();
+	}
+
+	header('Content-type: application/json'); //set tipe response jadi json
+	echo json_encode($response); //merubah respone menjadi JsonObject lalu dikirim
+}
+function login() {
+	//inisialisasi variabel yang akan ditampung dan diolah dengan query
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	global $conn;
+	global $response;
+	//inisialiasi query cek akun
+	$query = "SELECT * FROM user WHERE email = '$email'";
+	//pemanggilan fungsi mysqli_query untuk mengirimkan perintah sesuai parameter yang diisi
+	$result = $conn->query($query);
+	//pengkondisian saat fungsi mysqli_query berhasil atau gagal dieksekusi
+	if ($result->num_rows === 1) {
+		$result = $result->fetch_assoc();
+		if (password_verify($password, $result['password'])) {
+			$response = array(
+				'status' => TRUE,
+				'msg' => 'Login berhasil',
+				'data' => array(
+					'id' => $result['id'],
+					'nama' => $result['nama'],
+					'telp' => $result['telp'],
+					'email' => $result['email']
+				)
+			);
 		} else {
 			$response = array(
 				'status' => FALSE,
-				'msg' => 'Email tidak terdaftar!'
+				'msg' => 'Password salah!'
 			);
 		}
-		echo json_encode($response); //merubah respone menjadi JsonObject lalu dikirim
-		$conn->close();
-	} else if($method=='registrasi') {
-
+	} else {
+		$response = array(
+			'status' => FALSE,
+			'msg' => 'Email tidak terdaftar!'
+		);
 	}
+	$conn->close();
+}
+
+function registrasi($nama, $email, $password) {
+	
+}
+
+function update() {
+
 }
