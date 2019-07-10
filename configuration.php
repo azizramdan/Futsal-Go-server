aea36ee5b988461e5abbf694cdcc88eab16ee66c
azizramdan <azizramdan44@gmail.com>
2019-07-10 20:22:38 +0700

    fix connection failed

--- configuration.php
+++ configuration.php
@@ -1,9 +1,16 @@
 <?php
+//error_reporting(0);
 $host = "localhost"; // Nama hostnya
 $username = "root"; // Username
 $password = ""; // Password (Isi jika menggunakan password)
 $database = "futsalgo"; // Nama databasenya
 $conn = new mysqli($host, $username, $password, $database); // Koneksi ke MySQL
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $response = array(
+        'status' => FALSE,
+        'msg' => 'Connection failed!'
+    );
+    header('Content-type: application/json');
+	echo json_encode($response);
+    die();
 }
