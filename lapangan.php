a80ce4c0c49b4010ad201800164af5a8944d132a
Fauzan <fauzanadzima944@gmail.com>
2019-07-27 21:36:20 +0700

    edit query

--- lapangan.php
+++ lapangan.php
@@ -26,21 +26,36 @@ function index() {
     global $conn;
     global $response;
     
-    $query = "SELECT * FROM lapangan";
+    $query = "SELECT lapangan.*, admin.telp, admin.alamat, admin.latitude, admin.longitude, admin.bank, admin.nama_rekening, admin.no_rekening 
+                FROM lapangan 
+                INNER JOIN admin 
+                ON lapangan.id_admin = admin.id";
     $result = $conn->query($query);
     $data = array();
     if($result->num_rows > 0) {
         while($row = mysqli_fetch_array($result)){
             array_push($data, array(
+                // 'id' => $row[0], 
+                // 'nama' => $row[1], 
+                // 'harga' => $row[2], 
+                // 'telp' => $row[3],
+                // 'alamat'=>$row[4],
+                // 'longitude'=>$row[5],
+                // 'latitude'=>$row[6],
+                // 'foto'=>$row[7],
+                // 'email'=>$row[8]));
+
                 'id' => $row[0], 
-                'nama' => $row[1], 
-                'harga' => $row[2], 
-                'telp' => $row[3],
-                'alamat'=>$row[4],
-                'longitude'=>$row[5],
-                'latitude'=>$row[6],
-                'foto'=>$row[7],
-                'email'=>$row[8]));
+                'nama' => $row[2], 
+                'harga' => $row[3], 
+                'foto' => $row[4],
+                'telp'=>$row[5],
+                'alamat'=>$row[6],
+                'latitude'=>$row[7],
+                'longitude'=>$row[8],
+                'bank'=>$row[9],
+                'nama_rekening'=>$row[10],
+                'no_rekening'=>$row[11]));
         }
         $response = array(
             'status' => TRUE,
