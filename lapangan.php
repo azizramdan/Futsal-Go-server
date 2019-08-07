60610af4151b2290b92182f9b1d58b02dac16302
Fauzan <fauzanadzima944@gmail.com>
2019-08-07 11:12:41 +0700

    add filter lerlaris dan termurah

--- lapangan.php
+++ lapangan.php
@@ -25,26 +25,60 @@ if($_SERVER['REQUEST_METHOD']=='GET') {
 function index() {
     global $conn;
     global $response;
-    
-    $query = "SELECT lapangan.*, admin.telp, admin.alamat, admin.latitude, admin.longitude, admin.bank, admin.nama_rekening, admin.no_rekening 
+    $sort = $_GET['sort'];
+
+    if($sort == 'terlaris') {
+        $query = "SELECT
+                        lapangan.*,
+                        admin.telp,
+                        admin.alamat,
+                        admin.latitude,
+                        admin.longitude,
+                        admin.bank,
+                        admin.nama_rekening,
+                        admin.no_rekening,
+                        (SELECT COUNT(id) FROM pesanan WHERE id_lapangan = lapangan.id AND status = 'sudah') AS terlaris
+                    FROM
+                        lapangan, admin
+                    WHERE 
+                        lapangan.id_admin = admin.id
+                    ORDER BY terlaris DESC";
+
+    } else if($sort == 'termurah') {
+        $query = "SELECT
+                    lapangan.*,
+                    admin.telp,
+                    admin.alamat,
+                    admin.latitude,
+                    admin.longitude,
+                    admin.bank,
+                    admin.nama_rekening,
+                    admin.no_rekening
+                FROM
+                    lapangan, admin
+                WHERE lapangan.id_admin = admin.id
+                ORDER BY CONVERT(lapangan.harga, decimal) ASC";
+
+    } else {
+        $query = "SELECT 
+                    lapangan.*, 
+                    admin.telp, 
+                    admin.alamat, 
+                    admin.latitude, 
+                    admin.longitude, 
+                    admin.bank, 
+                    admin.nama_rekening, 
+                    admin.no_rekening 
                 FROM lapangan 
                 INNER JOIN admin 
                 ON lapangan.id_admin = admin.id";
+    }
+    
     $result = $conn->query($query);
     $data = array();
     if($result->num_rows > 0) {
         while($row = mysqli_fetch_array($result)){
             array_push($data, array(
-                // 'id' => $row[0], 
-                // 'nama' => $row[1], 
-                // 'harga' => $row[2], 
-                // 'telp' => $row[3],
-                // 'alamat'=>$row[4],
-                // 'longitude'=>$row[5],
-                // 'latitude'=>$row[6],
-                // 'foto'=>$row[7],
-                // 'email'=>$row[8]));
-
                 'id' => $row[0], 
                 'nama' => $row[2], 
                 'harga' => $row[3], 
