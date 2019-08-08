f33c8d36a335c4e84990cc1281ea9f8ab3527e7c
azizramdan <azizramdan44@gmail.com>
2019-08-08 12:08:39 +0700

    add index

---
+++ admin/pesanan.php
@@ -0,0 +1,94 @@
+<?php
+include_once "..\configuration.php";
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        get();
+        break;
+
+    case 'POST':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+function get() {
+    if(isset($_GET['method'])) {
+        switch ($_GET['method']) {
+            case 'index':
+                index();
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+}
+function index() {
+    global $conn;
+    $waktu_pilih = date("Y-m-d H:i:s");
+    $id_admin = $_GET['id_admin'];
+    $data = array();
+    $response;
+    $query = "SELECT
+                pesanan.id,
+                pesanan.waktu_pilih,
+                pesanan.metode_bayar,
+                pesanan.status,
+                lapangan.nama AS nama_lapangan,
+                user.nama AS nama_pemesan
+            FROM
+                pesanan,
+                lapangan,
+                admin,
+                user
+            WHERE
+                pesanan.waktu_pilih >= '$waktu_pilih' AND pesanan.id_lapangan = lapangan.id AND lapangan.id_admin = admin.id AND pesanan.id_user = user.id AND admin.id = '$id_admin'
+            ORDER BY CASE
+                pesanan.status WHEN 'belum' THEN 1 WHEN 'sudah' THEN 2 ELSE 3
+            END";
+
+    $result = $conn->query($query);
+    if($result->num_rows > 0) {
+		while($row = mysqli_fetch_assoc($result)){
+			$waktu_pilih_tanggal = strftime('%A, %e %B %Y', strtotime($row['waktu_pilih']));
+			$waktu_pilih_jam = date('H:i', strtotime($row['waktu_pilih'])) . ' - ' . date('H:i', strtotime($row['waktu_pilih'] . '+60 minutes'));
+			switch($row['status']) {
+				case 'belum':
+					$status = 'Belum bayar';
+					break;
+				case 'sudah':
+					$status = 'Sudah bayar';
+					break;
+				default:
+				$status = 'Dibatalkan';
+			}
+			array_push($data, array(
+				'id' => $row['id'],
+				'waktu_pilih_tanggal' => $waktu_pilih_tanggal,
+				'waktu_pilih_jam' => $waktu_pilih_jam,
+				'metode_bayar' => $row['metode_bayar'],
+				'status' => $status,
+				'nama_lapangan' => $row['nama_lapangan'],
+				'nama_pemesan' => $row['nama_pemesan']
+			));
+		}
+		$response = array(
+			'status' => TRUE,
+			'msg' => '',
+			'data' => $data
+		);
+	} else {
+		$response = array(
+			'status' => FALSE,
+			'msg' => 'Tidak ada data!'
+		);
+	}
+    $conn->close();
+    echo json_encode($response);
+}
+
