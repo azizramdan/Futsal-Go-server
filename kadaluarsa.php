9be187118d7cc309f6a0c871d05c2f3df3692300
azizramdan <azizramdan44@gmail.com>
2019-08-12 14:37:17 +0700

    add fungsi pengecekan pesanan kadaluarsa

---
+++ kadaluarsa.php
@@ -0,0 +1,22 @@
+<?php
+include_once "configuration.php";
+global $conn;
+$query = "UPDATE pesanan 
+				SET 
+					status = 'kadaluarsa'
+				WHERE 
+                    status = 'belum' AND waktu_pilih < NOW()";
+$result = $conn->query($query);
+if($result) {
+    $response = array(
+        'status' => TRUE,
+        'msg' => 'Update berhasil!'
+    );
+} else {
+    $response = array(
+        'status' => FALSE,
+        'msg' => 'Update gagal!'
+    );
+}
+$conn->close();
+echo json_encode($response);
